36968efcba58e3547bcd79970f1595b1f45b2ddb
GucciFlexus <victor2001rus@gmail.com>
2024-11-04 03:04:59 +0300

    DataSaver. Level fixes. UI fixes

--- Assets/Scripts/Infrastructure/Services/DataSaver.cs
+++ Assets/Scripts/Infrastructure/Services/DataSaver.cs
@@ -14,10 +14,23 @@ namespace Infrastructure.Services
 
         public void SaveResult()
         {
-            long scores = _scoresStorage.Scores;
-            //TODO wirte data in playerPref
+            int scores = (int)_scoresStorage.Scores;
+
+
+            int bestScore = PlayerPrefs.GetInt("BestScore", 0);
+            if (scores > bestScore)
+            {
+                PlayerPrefs.SetInt("BestScore", (int)scores);
+                Debug.Log($"<color=yellow>New high score: {scores}</color>");
+            }
+
+            
+            PlayerPrefs.SetInt("Score", (int)scores);
+            PlayerPrefs.Save();
+
             Debug.Log($"<color=green>Result is {scores}</color>");
         }
+
     }
 }
 
